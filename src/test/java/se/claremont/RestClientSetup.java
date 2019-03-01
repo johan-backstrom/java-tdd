bbdd8617763548efcee594d303fb24a2ee66602f
Johan Bäckström <johan.backstrom@claremont.se>
2019-03-01 08:56:51 +0100

    Added jackson for serialization and deserialization

---
+++ src/test/java/se/claremont/RestClientSetup.java
@@ -0,0 +1,36 @@
+package se.claremont;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
+
+import java.io.IOException;
+
+public class RestClientSetup {
+
+    public static void setupUnirest(){
+        Unirest.setDefaultHeader("Content-Type", "application/json");
+        Unirest.setObjectMapper(
+                new ObjectMapper() {
+                    @Override
+                    public <T> T readValue(String s, Class<T> aClass) {
+                        try {
+                            return new com.fasterxml.jackson.databind.ObjectMapper().readValue(s, aClass);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    @Override
+                    public String writeValue(Object o) {
+                        try {
+                            return new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(o);
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+    }
+
+}
